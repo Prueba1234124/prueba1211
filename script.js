e335e613835b42bebd5dada965a4c5405378dc2d
Prueba1234124 <andree.valenzuelam@gmail.com>
2026-05-31 13:00:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('titulo').innerText = data.titulo;
+    });
 
+document.getElementById('btn-bienvenida').addEventListener('click', () => {
+    alert('¡Gracias por visitar!');
+});
